e5cb5944b7f230272b129d20375f412b78a1167c
marekgluza <marekgluza@gmail.com>
2020-03-30 16:19:14 +0200

    package name

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gaussian_fermions',
+      version='0.1',
+      description='A set of functions useful for calculations of properties of fermions in a Gaussian regime.',
+      url='https://github.com/marekgluza/Gaussian_fermions.git',
+      author='Marek Gluza',
+      author_email='gluza@zedat.fu-berlin.de',
+      license='GPL-3.0',
+      packages=['gaussian_fermions'],
+      zip_safe=False)
